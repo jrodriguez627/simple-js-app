1de1831600b4437fbcbdced3d739be2ecbdc4420
jrodriguez627 <161751642+jrodriguez627@users.noreply.github.com>
2024-03-19 17:35:35 -0400

    "for" loop created for pokemon array
    If applied, this commit will include a "for" loop for my pokemon list, and shows which pokemon is the tallest of the three listed using "document.write".

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let pokemonList = [
     {name: "Squirtle", height: 1, types: ['water']},
-    {name: "Charmander", height: "2", types: ['fire']},
+    {name: "Charmander", height: 2, types: ['fire']},
     {name: "Pikachu", height: 1, types: ['electric']}
 ];
 
